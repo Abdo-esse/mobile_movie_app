f141f96ddeda44b48a8811b97eab3474b99b80f6
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-05-25 12:58:21 +0100

    test nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>welcom</Text>
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-4xl text-green-500 font-bold">Hello NativeWind</Text>
     </View>
   );
 }
